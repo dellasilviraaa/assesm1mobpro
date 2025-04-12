f7f7bf406f3da351f24a335885e68ff1a39025b8
Della Silvira <dellasilvira24@gmail.com>
2025-04-12 11:16:33 +0700

    membuat dua tampilan dan menambahkan gambar

--- app/src/main/java/com/dellas0095/assesmobpro1/MainActivity.kt
+++ app/src/main/java/com/dellas0095/assesmobpro1/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.dellas0095.assesmobpro1
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dellas0095.assesmobpro1.ui.theme.Assesmobpro1Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +38,68 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Assesmobpro1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
-
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+
+
+            )
+        }
+    ) { innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
+    }
 }
-
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
+fun ScreenContent(modifier: Modifier = Modifier) {
+    var nama by rememberSaveable { mutableStateOf("") }
+    var lokasi by rememberSaveable { mutableStateOf("") }
+    var layanan by rememberSaveable { mutableStateOf("Tambal Biasa") }
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    var showResult by rememberSaveable { mutableStateOf(false) }
+
+    val layananOptions = listOf("Tambal Biasa", "Ganti Ban", "Isi Angin")
+    val harga = when (layanan) {
+        "Tambal Biasa" -> 10000
+        "Ganti Ban" -> 30000
+        "Isi Angin" -> 5000
+        else -> 0
+    }
+
+    val context = LocalContext.current
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.banner_tambalban),
+            contentDescription = stringResource(R.string.banner),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
+        )
+    }
+}
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun MainScreenPreview() {
     Assesmobpro1Theme {
-        Greeting("Android")
+        MainScreen()
     }
 }
